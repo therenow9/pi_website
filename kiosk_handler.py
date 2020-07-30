00ef1eb91e1b5ccb6c0900ef39ec0bb96b6f2d20
therenow9 <60898774+therenow9@users.noreply.github.com>
2020-07-30 14:28:36 -0400

    Add files via upload

---
+++ kiosk_handler.py
@@ -0,0 +1,27 @@
+'''
+Pendant Automation
+Contact:(410) 939-7707
+
+@author: Jeremy Scheuerman
+
+@version: 1.0
+
+Created:7/29/20
+
+Last Updated:7/29/20
+
+Changes:N/A
+
+'''
+# Imports
+
+import subprocess;
+
+# Deployment Variables
+deploy_url = "https://www.pendantautomation.com/";
+
+while True:
+    v = subprocess.Popen("chromium-browser --kiosk " + deploy_url, shell=True);
+    # open up browser in kiosk mode
+    v.wait();
+    # wait for program to close then reopen
