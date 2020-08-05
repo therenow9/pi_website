6c3ac70418632b0874a885df5d7a260cfd78a82c
therenow9 <60898774+therenow9@users.noreply.github.com>
2020-08-05 12:16:24 -0400

    Add files via upload

--- kiosk_handler.py
+++ kiosk_handler.py
@@ -13,16 +13,17 @@ Last Updated:7/29/20
 Changes:N/A
 
 '''
-'''
 # Imports
 
-import subprocess;
 
+import subprocess;
 # Deployment Variables
 deploy_url = "https://www.pendantautomation.com/";
 
+# code
 while True:
-    v = subprocess.Popen("chromium-browser --fast --fast-start  --app=" + deploy_url, shell=True);
+    v = subprocess.Popen("chromium-browser --incognito --fast --start-fullscreen --fast-start --app= " + deploy_url, shell=True);
     # open up browser in kiosk mode
     v.wait();
     # wait for program to close then reopen
+
